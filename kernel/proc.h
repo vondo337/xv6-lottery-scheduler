9acedd4acbd2071ab0c0f4f38dbaccecf2e7d35d
vondo337 <ishmamahmed603@gmail.com>
2026-08-26 01:23:27 +0600

    Added ticket field to the process structure

--- kernel/proc.h
+++ kernel/proc.h
@@ -91,6 +91,7 @@ struct proc {
 
   // wait_lock must be held when using this:
   struct proc *parent; // Parent process
+  int tickets;
 
   // these are private to the process, so p->lock need not be held.
   uint64 kstack;               // Virtual address of kernel stack
