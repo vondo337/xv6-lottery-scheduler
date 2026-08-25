40de3ca9a55586a768a8a4102ae977641f78b72f
vondo337 <ishmamahmed603@gmail.com>
2026-08-26 03:04:15 +0600

    Declared runtime ticks to proc.h

--- kernel/proc.h
+++ kernel/proc.h
@@ -88,10 +88,11 @@ struct proc {
   int killed;           // If non-zero, have been killed
   int xstate;           // Exit status to be returned to parent's wait
   int pid;              // Process ID
+  int tickets;
+  uint64 runtime_ticks;
 
   // wait_lock must be held when using this:
   struct proc *parent; // Parent process
-  int tickets;
 
   // these are private to the process, so p->lock need not be held.
   uint64 kstack;               // Virtual address of kernel stack
